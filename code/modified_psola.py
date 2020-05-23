7025a222556bc497a52c977b00fc11fd0232c2cf
LuFernandez <lu.gfernandez@gmail.com>
2020-05-22 23:17:39 -0300

    Update modified_psola.py

--- code/modified_psola.py
+++ code/modified_psola.py
@@ -22,7 +22,7 @@ def modified_psola(x,pitch_samples, speed, is_frame_speech, frame_len):
     total_time = len(x)
     total_new_time = ceil(total_time*(1/speed))
     new_audio = np.zeros(total_new_time)
-    peaks, _ = find_peaks(x, height=0, distance = pitch_samples-int(0.16*pitch_samples))
+    peaks, _ = find_peaks(x, height=0, distance = pitch_samplePAINTs-int(0.16*pitch_samples))
     #plot_peaks(x,peaks) # debugging
     w = np.hamming(2*pitch_samples+1)
     
@@ -37,8 +37,8 @@ def modified_psola(x,pitch_samples, speed, is_frame_speech, frame_len):
     pitch_centered_modified = []
     for i, centered in enumerate(pitch_centered):
         if speed >= 1:    # funciona bien para x1,x2,x4, falta agregar lo demas
-            num, den = speed.as_integer_ratio()
-            if den > i%num:
+            num, den = speed.as_integer_ratio() # 3 / 2 = > num 3 , den 2 ;  4 1 
+            if den > i%num: # 
                 pitch_centered_modified.append(pitch_centered[i])
 
     for i, centered in enumerate(pitch_centered_modified):
@@ -47,6 +47,8 @@ def modified_psola(x,pitch_samples, speed, is_frame_speech, frame_len):
             new_audio[t_-pitch_samples:t_+pitch_samples+1] += centered.samples_windowed
     
     # me gustaria saber el indice de la ventana de 20ms
+    # aca falta descartar ventanas
+    # con el factor de speed 
     for i in range(0,len(x),frame_len):
         if not is_frame_speech[i]:
           t_ = ceil(i+frame_len//2 * (1/speed))
