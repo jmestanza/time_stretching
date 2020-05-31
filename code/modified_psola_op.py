a94e2ef241014503e7bb160ffa18f7949f10dfbc
Joaquin Mestanza <jmestanza@itba.edu.ar>
2020-05-31 00:41:26 -0300

    corregi error de las pitch zones

--- code/modified_psola_op.py
+++ code/modified_psola_op.py
@@ -33,20 +33,60 @@ def search_pitch_samples(peak,indexes):
     return b
             
 
-def find_max_probability_f0(reg_number,start,end,indexes,sample_f0):
+def find_max_probability_f0(start,end,indexes,sample_f0):
     f0s_in_reg = []
     for i,idx in enumerate(indexes):
         if start<= idx and idx <= end:
-        #   if sample_f0[idx] != 0:
             f0s_in_reg.append(sample_f0[i])
     hist, bin_edges = np.histogram(np.array(f0s_in_reg), bins=150) 
-    #plt.plot(hist)    
-    #plt.xlabel("Distribucion de f0_hat en samples")
-    #plt.ylabel("Frecuencia")
-    #plt.show()
     return int(bin_edges[np.argmax(hist)])
 
-def modified_psola(x, indexes, f0_in_samples, percent,percent_pitch, speed, is_frame_speech, regions,pitch_version, pitch_dif_mode,unvoiced_samples=360):
+
+def pitch_var_mode(start,end,indexes,f0_in_samples,x,peak_percent,percent_pitch):
+    pitch_centered = []
+    p_samples = find_max_probability_f0(start,end,indexes,f0_in_samples)
+    peaks, _ = find_peaks(x, distance = p_samples - p_samples*peak_percent)
+    for i,peak in enumerate(peaks):
+        if start <= peak and peak <= end:
+            curr_pitch = f0_in_samples[search_pitch_samples(peak, indexes)]# busco cuanto pitch tiene este peak
+            curr_pitch = int(curr_pitch*percent_pitch)
+            w_sonora = np.hanning(2*curr_pitch+1)
+            if peak-curr_pitch>=0 and peak+curr_pitch <= len(x) - 1: 
+                samples_windowed = x[peak-curr_pitch:peak+curr_pitch+1]*w_sonora
+                pitch_centered.append(pitch_centered_segment(curr_pitch, peak, samples_windowed))
+    return pitch_centered
+
+def pitch_cte_mode(start,end,indexes,f0_in_samples,x,peak_percent,percent_pitch):
+    pitch_centered = []
+    p_samples = find_max_probability_f0(start,end,indexes,f0_in_samples)
+    peaks, _ = find_peaks(x, distance = p_samples - p_samples*peak_percent)
+    new_p_samples = int(percent_pitch*p_samples) # cte
+    w_sonora = np.hanning(2*new_p_samples+1)  # cte
+    for i,peak in enumerate(peaks):
+        if start <= peak and peak <= end:
+            if peak-new_p_samples>=0 and peak+new_p_samples <= len(x) - 1: 
+                samples_windowed = x[peak-new_p_samples:peak+new_p_samples+1]*w_sonora
+                pitch_centered.append(pitch_centered_segment(new_p_samples, peak, samples_windowed))
+    return pitch_centered
+
+
+def get_unvoiced_windowed_samples(unvoiced_samples,start,end,x):
+    i = 0
+    leap = unvoiced_samples
+    unvoiced_centered = []
+    right_lim = start
+    unvoiced_centered = []
+    while right_lim  < end:
+        middle = leap 
+        t = start + middle + i*leap
+        right_lim = t + leap
+        curr_w = x[t - leap : t + leap + 1]
+        w_n_s = np.hanning(len(curr_w))
+        unvoiced_centered.append(pitch_centered_segment(leap,t,curr_w*w_n_s))
+        i += 1
+    return unvoiced_centered
+
+def modified_psola(x, indexes, f0_in_samples, peak_percent,percent_pitch, speed, is_frame_speech, regions,pitch_version, pitch_dif_mode,unvoiced_samples=360):
 
     x = x[:len(is_frame_speech)]
     total_time = len(x)
@@ -57,63 +97,19 @@ def modified_psola(x, indexes, f0_in_samples, percent,percent_pitch, speed, is_f
 
     pitch_zones = []
     unvoiced_zones = []
-    for reg_number, reg in enumerate(regions):
+    for reg in regions:
         start,end = reg
         if is_frame_speech[start]:
-            last_peak = None
             pitch_centered = []
-            if pitch_version == "pitch_variable": # pitch variable
-                is_first_time = True
-                p_samples = find_max_probability_f0(reg_number,start,end,indexes,f0_in_samples)
-                peaks, _ = find_peaks(x, distance = p_samples - p_samples*percent)
-                peaks.sort()
-                for i,peak in enumerate(peaks):
-                    if start <= peak and peak <= end:
-                        if i == 0: 
-                            f0_idx = search_pitch_samples(peak, indexes) # busco cuanto pitch tiene este peak
-                            curr_pitch = f0_in_samples[f0_idx]
-                            curr_pitch = int(curr_pitch*percent_pitch)
-                            w_sonora = np.hanning(2*curr_pitch+1)
-                            if peak-curr_pitch>=0 and peak+curr_pitch <= len(x) - 1: 
-                                samples_windowed = x[peak-curr_pitch:peak+curr_pitch+1]*w_sonora
-                                pitch_centered.append(pitch_centered_segment(curr_pitch, peak, samples_windowed))
-                                pitch_zones.append([peak-curr_pitch,peak+curr_pitch])
-                            last_peak = peak[0]
-                        else:
-                            if (peaks[i] - last_peak) > (p_samples - p_samples* percent):
-                                f0_idx = search_pitch_samples(peak, indexes) # busco cuanto pitch tiene este peak
-                                curr_pitch = f0_in_samples[f0_idx]
-                                curr_pitch = int(curr_pitch*percent_pitch)
-                                w_sonora = np.hanning(2*curr_pitch+1)
-                                if peak-curr_pitch>=0 and peak+curr_pitch <= len(x) - 1: 
-                                    samples_windowed = x[peak-curr_pitch:peak+curr_pitch+1]*w_sonora
-                                    pitch_centered.append(pitch_centered_segment(curr_pitch, peak, samples_windowed))
-                                    pitch_zones.append([peak-curr_pitch,peak+curr_pitch])
-                                last_peak = peaks[i]
-                        
-            #-ASI ESTABA ANTES CON FLANGE Y EL OTRO CAMBIO:
-            if pitch_version == "pitch_cte": # pitch cte
-                is_first_time = True
-                p_samples = find_max_probability_f0(reg_number,start,end,indexes,f0_in_samples)
-                p_samples = int(percent_pitch*p_samples) 
-                peaks, _ = find_peaks(x, distance = p_samples - p_samples*percent)
-                w_sonora = np.hanning(2*p_samples+1)
-                peaks.sort()
-                for i,peak in enumerate(peaks):
-                    if start <= peak and peak <= end:
-                        if i == 0:
-                            if peak-p_samples>=0 and peak+p_samples <= len(x) - 1: 
-                                samples_windowed = x[peak-p_samples:peak+p_samples+1]*w_sonora
-                                pitch_centered.append(pitch_centered_segment(p_samples, peak, samples_windowed))
-                                pitch_zones.append([peak-p_samples,peak+p_samples])
-                                last_peak = peak[0]
-                        else:   
-                            if (peaks[i] - last_peak) > (p_samples - p_samples* percent):
-                                if peak-p_samples>=0 and peak+p_samples <= len(x) - 1: 
-                                    samples_windowed = x[peak-p_samples:peak+p_samples+1]*w_sonora
-                                    pitch_centered.append(pitch_centered_segment(p_samples, peak, samples_windowed))
-                                    pitch_zones.append([peak-p_samples,peak+p_samples])
-                                last_peak = peak[i]
+            is_first_time = True
+
+            if pitch_version == "pitch_var": # pitch variable
+                pitch_centered = pitch_var_mode(start,end,indexes,f0_in_samples,x,peak_percent,percent_pitch)
+            elif pitch_version == "pitch_cte": # pitch cte
+                pitch_centered = pitch_cte_mode(start,end,indexes,f0_in_samples,x,peak_percent,percent_pitch)
+            else:
+                return None
+                
             for i, centered in enumerate(pitch_centered):
                 t_ = ceil(centered.t * (1/speed))
                 
@@ -154,23 +150,11 @@ def modified_psola(x, indexes, f0_in_samples, percent,percent_pitch, speed, is_f
                             curr_t_shifted = curr_t_+ distance 
                             if curr_t_shifted-centered.pitch>= 0 and curr_t_shifted+centered.pitch < len(new_audio):
                                 new_audio[curr_t_shifted-centered.pitch:curr_t_shifted+centered.pitch+1] += centered.samples_windowed
+                                pitch_zones.append([curr_t_shifted-centered.pitch,curr_t_shifted+centered.pitch])
     
-        else:# si es no sonora
-            #w_no_sonora = np.hanning(2*unvoiced_samples+1)
-            #samp = int(end-start)
-            i = 0
-            leap = unvoiced_samples
-            unvoiced_centered = []
-            right_lim = start
-            while right_lim  < end:
-                middle = leap 
-                t = start + middle + i*leap
-                right_lim = t + leap
-                curr_w = x[t - leap : t + leap + 1]
-                w_n_s = np.hanning(len(curr_w))
-                unvoiced_centered.append(pitch_centered_segment(leap,t,curr_w*w_n_s))
-                i += 1
-
+        else:
+            unvoiced_centered = get_unvoiced_windowed_samples(unvoiced_samples,start,end,x)
+            
             for i,unv_cent in enumerate(unvoiced_centered): 
                 t_ = ceil(unv_cent.t*(1/speed))
                 if speed == 1.0 or speed == 2.0 or speed == 4.0:
@@ -194,11 +178,4 @@ def modified_psola(x, indexes, f0_in_samples, percent,percent_pitch, speed, is_f
                             new_audio[curr_t_shifted-unv_cent.pitch:curr_t_shifted+unv_cent.pitch+1] += unv_cent.samples_windowed
                             unvoiced_zones.append([t_-unv_cent.pitch,t_+unv_cent.pitch])
 
-                    # if i%num == 0: # si es 1.5 => 3, 2 
-                    #     curr_t_ = t_ # cada 3 actualizo el t_
-                    # if den> i%num:
-                    #     curr_t_shifted = curr_t_+(i%num)*unvoiced_samples 
-                    #     if curr_t_shifted-unvoiced_samples>=0 and curr_t_shifted+unvoiced_samples < len(new_audio):
-                    #             new_audio[curr_t_shifted-unvoiced_samples:curr_t_shifted+unvoiced_samples+1] += unv_cent.samples_windowed
-                    #             unvoiced_zones.append([curr_t_shifted-unvoiced_samples,curr_t_shifted+unvoiced_samples])
     return new_audio, pitch_zones, unvoiced_zones
